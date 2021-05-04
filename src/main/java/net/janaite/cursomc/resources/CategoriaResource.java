628c7f0245d18e77160fcdca194eb56a770e5aa6
janaite <janaite@gmail.com>
2021-05-04 17:51:37 -0300

    adjustments /categoria/{id} endpoint

--- src/main/java/net/janaite/cursomc/resources/CategoriaResource.java
+++ src/main/java/net/janaite/cursomc/resources/CategoriaResource.java
@@ -1,10 +1,5 @@
 package net.janaite.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,11 +16,11 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaService service;
-	
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 }
--- src/main/java/net/janaite/cursomc/services/CategoriaService.java
+++ src/main/java/net/janaite/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import net.janaite.cursomc.domain.Categoria;
 import net.janaite.cursomc.repositories.CategoriaRepository;
+import net.janaite.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				String.format("Object not found! Id: %d, type: %s", id, Categoria.class.getName())));
 	}
-	
+
 }
