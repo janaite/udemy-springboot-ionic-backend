e08e9a43252d4d14097938f4ef66355d6b7c911e
janaite <janaite@gmail.com>
2021-05-04 12:21:17 -0300

    First domain class

--- src/main/java/net/janaite/cursomc/resources/CategoriaResource.java
+++ src/main/java/net/janaite/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package net.janaite.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import net.janaite.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST ok!!!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		return List.of(cat1, cat2);
 	}
 	
 }
