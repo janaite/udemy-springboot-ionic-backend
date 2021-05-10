66dc36f425fd26bb91839d53e92e721412c97ab9
janaite <janaite@gmail.com>
2021-05-10 17:53:55 -0300

    Categoria remove with DELETE

--- src/main/java/net/janaite/cursomc/services/CategoriaService.java
+++ src/main/java/net/janaite/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package net.janaite.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import net.janaite.cursomc.domain.Categoria;
 import net.janaite.cursomc.repositories.CategoriaRepository;
+import net.janaite.cursomc.services.exceptions.DataIntegrityException;
 import net.janaite.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,4 +32,15 @@ public class CategoriaService {
 		find(obj.getId());	// verify if id exists
 		return repo.save(obj);
 	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Delete Not possible"); 
+		}
+	}
+	
 }
