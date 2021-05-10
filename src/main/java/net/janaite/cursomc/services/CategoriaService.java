9845ce238930bfd1686f6d51069301c1d83a32cc
janaite <janaite@gmail.com>
2021-05-10 18:31:52 -0300

    Categoria with pagination

--- src/main/java/net/janaite/cursomc/services/CategoriaService.java
+++ src/main/java/net/janaite/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import net.janaite.cursomc.domain.Categoria;
@@ -28,24 +31,28 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
-		find(obj.getId());	// verify if id exists
+		find(obj.getId()); // verify if id exists
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
 		try {
-		repo.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Delete Not possible"); 
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Delete Not possible");
 		}
 	}
-	
+
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
 }
