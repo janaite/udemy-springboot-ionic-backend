7346e087bf019fabc165f66ab67324e195355ed2
janaite <janaite@gmail.com>
2021-05-10 14:40:44 -0300

    pedidos/{id} endpoint

---
+++ src/main/java/net/janaite/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package net.janaite.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.janaite.cursomc.domain.Pedido;
+import net.janaite.cursomc.repositories.PedidoRepository;
+import net.janaite.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				String.format("Object not found! Id: %d, type: %s", id, Pedido.class.getName())));
+	}
+
+}
