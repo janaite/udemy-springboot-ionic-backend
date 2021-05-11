d7825a72bce4fbf62d12e400e53c6887520fdf4a
janaite <janaite@gmail.com>
2021-05-11 14:28:03 -0300

    annotation @ClienteInsert, email unique

--- src/main/java/net/janaite/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/net/janaite/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import net.janaite.cursomc.domain.Cliente;
 import net.janaite.cursomc.domain.enums.TipoCliente;
 import net.janaite.cursomc.dto.ClienteNewDTO;
+import net.janaite.cursomc.repositories.ClienteRepository;
 import net.janaite.cursomc.resources.exception.FieldMessage;
 import net.janaite.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -32,6 +39,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 				list.add(new FieldMessage("cpfOuCnpj", "invalid CNPJ"));
 			}
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email not unique"));
 		}
 
 		// Convert a FiledMessage list into spring framework compatible list of errors
