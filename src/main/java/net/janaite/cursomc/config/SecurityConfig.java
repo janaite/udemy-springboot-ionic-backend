6910da22499c739c677bcea967f538c8ca2248d6
janaite <janaite@gmail.com>
2021-05-17 18:47:46 -0300

    Authorization with JWT bearer token

--- src/main/java/net/janaite/cursomc/config/SecurityConfig.java
+++ src/main/java/net/janaite/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import net.janaite.cursomc.security.JWTAuthenticationFilter;
+import net.janaite.cursomc.security.JWTAuthorizationFilter;
 import net.janaite.cursomc.security.JWTUtil;
 
 @Configuration
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
