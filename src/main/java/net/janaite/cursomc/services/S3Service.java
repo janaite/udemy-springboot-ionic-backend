43c4d6790dc8e59d8540c27f59f02531fb0b8853
janaite <janaite@gmail.com>
2021-05-18 17:49:01 -0300

    Sending photo to AWS S3 through endpoint

--- src/main/java/net/janaite/cursomc/services/S3Service.java
+++ src/main/java/net/janaite/cursomc/services/S3Service.java
@@ -1,16 +1,19 @@
 package net.janaite.cursomc.services;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Service
@@ -24,19 +27,31 @@ public class S3Service {
 	@Value("${s3.bucket}")
 	private String bucketName;
 
-	public void uploadFile(String localFilePath) {
+	public URI uploadFile(MultipartFile multipartFile) {
 		try {
-			File file = new File(localFilePath);
-			
+			String fileName = multipartFile.getOriginalFilename();
+			InputStream input = multipartFile.getInputStream();
+			String contentType = multipartFile.getContentType();
+
+			return uploadFile(input, fileName, contentType);
+		} catch (IOException e) {
+			throw new RuntimeException("I/O Error: " + e.getMessage());
+		}
+	}
+
+	public URI uploadFile(InputStream input, String fileName, String contentType) {
+		try {
+			ObjectMetadata metadata = new ObjectMetadata();
+			metadata.setContentType(contentType);
+
 			LOG.info("iniciando upload");
-			s3client.putObject(new PutObjectRequest(bucketName, "teste", file));
+			s3client.putObject(new PutObjectRequest(bucketName, fileName, input, metadata));
 			LOG.info("upload concluido");
-			
-		} catch (AmazonServiceException e) {
-			LOG.warn("AmazonServiceException: " + e.getErrorMessage());
-			LOG.warn("Status Code: " + e.getErrorCode());
-		} catch (AmazonClientException e) {
-			LOG.warn("AmazonClientException: " + e.getMessage());
+
+			return s3client.getUrl(bucketName, fileName).toURI();
+
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Erro when converting URL to URI");
 		}
 	}
 }
